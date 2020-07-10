3fe9c5f219f6b1cbcd5ecb284255fe5a88b4fd95
YoungSik <YoungSik@DESKTOP-G17ONCH>
2020-07-10 22:43:39 +0900

    index.jsp (page 수정) css > index-section.css 추가

--- test/WebContent/WEB-INF/user.sql
+++ test/WebContent/WEB-INF/user.sql
@@ -1,13 +1,12 @@
 --회원들의 정보가 담긴 테이블
 CREATE TABLE user_info(
-	userNum NUMBER PRIMARY KEY,
 	userName VARCHAR2(10) NOT NULL,
-	userId VARCHAR2(20) NOT NULL,
+	userId VARCHAR2(20) PRIMARY KEY NOT NULL,
 	userPw NUMBER NOT NULL,
 	userGender VARCHAR2(4),
 	userPhone VARCHAR2(15), 
 	userEmail VARCHAR2(20)
 );
---회원 가입시 회원 번호를 생성할 시퀀스
-CREATE SEQUENCE user_info_seq;
+
+
 
